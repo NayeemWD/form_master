07a75c24d6e5458b5f979f1ade833f9f5126edc4
NayeemWD <msnayeem333@gmail.com>
2024-10-08 19:56:34 +0600

    first commit

---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,16 @@
+
+const SimpleForm = () => {
+    return (
+        <div>
+            <form >
+                <input type="text" />
+                <br />
+                <button>Submit</button>
+
+            </form>
+            
+        </div>
+    );
+};
+
+export default SimpleForm;
